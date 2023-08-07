37f7cb8d32f782acc0a5b081d91031ccc7189573
qthierry <qthierry@student.42.fr>
2023-08-07 22:35:45 +0200

    test door doesn't work

--- Cub3d_bonus/src/wall_hit.c
+++ Cub3d_bonus/src/wall_hit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   wall_hit.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jvigny <jvigny@student.42.fr>              +#+  +:+       +#+        */
+/*   By: qthierry <qthierry@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/30 16:04:05 by qthierry          #+#    #+#             */
-/*   Updated: 2023/08/06 19:03:21 by jvigny           ###   ########.fr       */
+/*   Updated: 2023/08/07 22:26:56 by qthierry         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 // xy (1, 1)
 static t_ray	_get_wall_hit_se(t_fvector2 fpos,
-								t_map **map, float angle, t_vector2 map_size)
+								t_map **map, float angle, t_vector2 map_size, float d)
 {
 	t_fvector2	step;
 	t_fvector2	comp;
@@ -37,15 +37,21 @@ static t_ray	_get_wall_hit_se(t_fvector2 fpos,
 				{
 					if (comp.y + step.y / 2 < (int)comp.y + 1)
 					{
-						if (comp.y + step.y / 2 < (int)comp.y + 0.5)
+						// if (comp.y + step.y / 2 < (int)comp.y + 0.5)
 							r = (comp.y + step.y / 2) - (int)comp.y;
-						else
-							r = (int)(comp.y + 1) - (comp.y + step.y / 2);
-						float add = (r * tan((float)map[(int)(comp.y)][map_pos.x].door_percent * TO_RADIAN));
-						printf("r : %f test %f	angle : %d	%f\n",r, add, map[(int)(comp.y)][map_pos.x].door_percent, 0.5 * sin((float)map[(int)(comp.y)][map_pos.x].door_percent * TO_RADIAN));
-						if (0.5 * sin((float)map[(int)(comp.y)][map_pos.x].door_percent * TO_RADIAN) >= add)
+						// else
+							// r = (int)(comp.y + 1) - (comp.y + step.y / 2);
+						// float add = (r * tan((float)map[(int)(comp.y)][map_pos.x].door_percent * TO_RADIAN));
+						float c = (r * sin((float)map[(int)(comp.y)][map_pos.x].door_percent * TO_RADIAN))
+							/ sin((180 - (float)map[(int)(comp.y)][map_pos.x].door_percent - (180 - d)) * TO_RADIAN);
+						float h = sin((180 - d) * TO_RADIAN) * c;
+						float b = cos((180 - d) * TO_RADIAN) * c;
+						printf("r	: %f	angle : %d	h : %f	b : %f	c : %f\n", r, map[(int)(comp.y)][map_pos.x].door_percent
+								, h, b, c);
+						if (1 * sin((float)map[(int)(comp.y)][map_pos.x].door_percent * TO_RADIAN) >= b
+							&& 1 * cos((float)map[(int)(comp.y)][map_pos.x].door_percent * TO_RADIAN) >= h)
 						{
-							return ((t_ray){{map_pos.x + 0.5 + add, comp.y + step.y / 2}, e_west});
+							return ((t_ray){{map_pos.x + 0.5 + h, comp.y + step.y / 2 + b}, e_west});
 						}
 					}
 					// if (comp.y + step.y / 2 < (int)comp.y + map[(int)(comp.y)][map_pos.x].door_percent / 100.f
@@ -244,13 +250,16 @@ static t_ray	_get_wall_hit_nw(t_fvector2 fpos,
 t_ray	get_wall_hit(t_fvector2 fpos, t_map **map, float angle, t_vector2 map_size)
 {
 	t_vector2	sign;
+	float		d_angle;
 
 	if (fpos.x < 0 || fpos.y < 0 || fpos.x > map_size.x || fpos.y > map_size.y)
 		return((t_ray){{-1, -1}, -1});
 	sign = get_sign(angle);
+	// printf("angle : %f\n",angle);
+	d_angle = angle;
 	angle = fabs(tan(angle * TO_RADIAN));
 	if (sign.x == 1 && sign.y == 1)
-		return (_get_wall_hit_se(fpos, map, angle, map_size));
+		return (_get_wall_hit_se(fpos, map, angle, map_size, d_angle));
 	else if (sign.x == 1 && sign.y == -1)
 		return (_get_wall_hit_ne(fpos, map, angle, map_size));
 	else if (sign.x == -1 && sign.y == 1)

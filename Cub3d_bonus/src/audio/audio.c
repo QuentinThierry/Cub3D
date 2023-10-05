062729319e23f2ef1d87b673cc0fdae713d40b65
Quentin THIERRY <qthierry@bess-f3r4s10.clusters.42paris.fr>
2023-10-05 15:56:34 +0200

    [FIX] min sound number is now 2, to let the narrator an array cell

--- Cub3d_bonus/src/audio/audio.c
+++ Cub3d_bonus/src/audio/audio.c
@@ -6,7 +6,7 @@
 /*   By: qthierry <qthierry@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 17:16:22 by qthierry          #+#    #+#             */
-/*   Updated: 2023/10/04 14:39:26 by qthierry         ###   ########.fr       */
+/*   Updated: 2023/10/05 15:41:25 by qthierry         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 bool	init_audio(t_game *game, t_music_name *music_file, int nb_music)
 {
 	InitAudioDevice();
-	if (!IsAudioDeviceReady() || NB_MAX_SOUNDS <= 0)
+	if (!IsAudioDeviceReady() || NB_MAX_SOUNDS <= 1)
 		return (print_error("Audio init failed\n", 1), false);
 	game->music_array = ft_calloc(NB_MAX_SOUNDS, sizeof(t_music_game));
 	if (game->music_array == NULL)

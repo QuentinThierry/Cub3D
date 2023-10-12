605b0b55ffa9e61784ae416d78a0168c185cea0d
J0hann3 <jvigny@student.42.fr>
2023-10-12 19:17:31 +0200

    [NORM] parse_music

--- Cub3d_bonus/src/parsing/parse_music.c
+++ Cub3d_bonus/src/parsing/parse_music.c
@@ -6,7 +6,7 @@
 /*   By: jvigny <jvigny@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 14:32:29 by jvigny            #+#    #+#             */
-/*   Updated: 2023/10/11 15:34:46 by jvigny           ###   ########.fr       */
+/*   Updated: 2023/10/12 19:12:24 by jvigny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 #define READ_SIZE 100
 
-static bool	check_format(char *filename)
+static bool	_check_format(char *filename)
 {
 	int	len;
 
@@ -26,35 +26,43 @@ static bool	check_format(char *filename)
 	return (true);
 }
 
-static bool	load_subtitle(t_music_name *music)
+static char	*_read_subtitle(int fd, int *ret)
+{
+	char	*tmp;
+	char	*res;
+	char	buffer[READ_SIZE + 1];
+
+	res = NULL;
+	buffer[READ_SIZE] = 0;
+	*ret = read(fd, buffer, READ_SIZE);
+	while (*ret > 0)
+	{
+		if (*ret != READ_SIZE)
+			buffer[*ret] = 0;
+		tmp = ft_strjoin(res, buffer);
+		free(res);
+		if (tmp == NULL)
+			return (print_error("Malloc failed\n", 1), close(fd), NULL);
+		res = tmp;
+		*ret = read(fd, buffer, READ_SIZE);
+	}
+	return (res);
+}
+
+static bool	_load_subtitle(t_music_name *music)
 {
 	int		fd;
 	int		ret;
-	char	buffer[READ_SIZE + 1];
 	char	*res;
-	char	*tmp;
 
-	if (!check_format(music->subtitle))
+	if (!_check_format(music->subtitle))
 		return (false);
 	fd = open(music->subtitle, O_RDONLY);
 	if (fd == -1)
 		return (print_error(NULL, 0), false);
-	res = NULL;
-	buffer[READ_SIZE] = 0;
-	ret = read(fd, buffer, READ_SIZE);
-	while (ret > 0)
-	{
-		if (ret != READ_SIZE)
-			buffer[ret] = 0;
-		tmp = ft_strjoin(res, buffer);
-		free(res);
-		if (tmp == NULL)
-			return (print_error("Malloc failed\n", 1), close(fd), false);
-		res = tmp;
-		if (ret != READ_SIZE)
-			break ;
-		ret = read(fd, buffer, READ_SIZE);
-	}
+	res = _read_subtitle(fd, &ret);
+	if (res == NULL)
+		return (false);
 	free(music->subtitle);
 	music->subtitle = res;
 	close(fd);
@@ -89,11 +97,52 @@ static bool	_is_existing(t_game *game, char symbol, enum e_orientation orient)
 	return (false);
 }
 
+static bool	_get_subtitle(char *str, int *i, t_music_name *music)
+{
+	int		len;
+	char	*filename;
+
+	*i += skip_whitespace(str + *i);
+	if (str[*i] == '\0')
+		return (print_error("Empty texture\n", 1), false);
+	len = find_next_wsp(str + *i, 0);
+	if (len >= 0 && (str[*i + len] == ' ' || str[*i + len] == '\t'
+			|| str[*i + len] == '\v' || str[*i + len] == '\n'
+			|| str[*i + len] == '\f' || str[*i + len] == '\r'))
+		str[*i + len] = '\0';
+	filename = ft_strdup(str + *i);
+	if (filename == NULL)
+		return (print_error("malloc failed\n", 1), false);
+	*i += len + 1;
+	music->subtitle = filename;
+	if (!_load_subtitle(music))
+		return (false);
+	return (true);
+}
+
+static char	*_get_filename(char *str, int *i)
+{
+	int		len;
+	char	*filename;
+
+	*i += skip_whitespace(str + *i);
+	if (str[*i] == '\0')
+		return (print_error("Empty texture\n", 1), NULL);
+	len = find_next_wsp(str + *i, 0);
+	if (len >= 0 && (str[*i + len] == ' ' || str[*i + len] == '\t'
+			|| str[*i + len] == '\v' || str[*i + len] == '\n'
+			|| str[*i + len] == '\f' || str[*i + len] == '\r'))
+		str[*i + len] = '\0';
+	filename = ft_strdup(str + *i);
+	if (filename == NULL)
+		return (print_error("malloc failed\n", 1), NULL);
+	*i += len + 1;
+	return (filename);
+}
+
 bool	find_music(t_game *game, char *str, enum e_orientation orient, int i)
 {
-	char	*filename;
 	void	*tmp;
-	int		len;
 	int		index;
 
 	index = game->nb_music;
@@ -111,39 +160,14 @@ bool	find_music(t_game *game, char *str, enum e_orientation orient, int i)
 	}
 	game->file_music[index].orient = orient;
 	game->file_music[index].symbol = *(str - 1);
-	i += skip_whitespace(str + i);
-	if (str[i] == '\0')
-		return (print_error("Empty texture\n", 1), false);
-	len = find_next_wsp(str + i, 0);
-	if (len >= 0 && (str[i + len] == ' ' || str[i + len] == '\t'
-			|| str[i + len] == '\v' || str[i + len] == '\n'
-			|| str[i + len] == '\f' || str[i + len] == '\r'))
-		str[i + len] = '\0';
-	filename = ft_strdup(str + i);
-	if (filename == NULL)
-		return (print_error("malloc failed\n", 1), false);
-	i += len + 1;
-	game->file_music[index].filename = filename;
+	game->file_music[index].filename = _get_filename(str, &i);
+	if (game->file_music[index].filename == NULL)
+		return (false);
 	if (orient == e_narrator || orient == e_narrator_receptacle
 		|| orient == e_narrator_receptacle_complete
 		|| orient == e_narrator_receptacle_complete)
-	{
-		i += skip_whitespace(str + i);
-		if (str[i] == '\0')
-			return (print_error("Empty texture\n", 1), false);
-		len = find_next_wsp(str + i, 0);
-		if (len >= 0 && (str[i + len] == ' ' || str[i + len] == '\t'
-				|| str[i + len] == '\v' || str[i + len] == '\n'
-				|| str[i + len] == '\f' || str[i + len] == '\r'))
-			str[i + len] = '\0';
-		filename = ft_strdup(str + i);
-		if (filename == NULL)
-			return (print_error("malloc failed\n", 1), false);
-		i += len + 1;
-		game->file_music[index].subtitle = filename;
-		if (!load_subtitle(&game->file_music[index]))
+		if (!_get_subtitle(str, &i, &game->file_music[index]))
 			return (false);
-	}
 	if (orient == e_music_receptacle || orient == e_narrator_receptacle)
 		return (find_music(game, str, orient + 1, i));
 	return (true);

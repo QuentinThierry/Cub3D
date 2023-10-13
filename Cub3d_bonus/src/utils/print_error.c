199acfd21630010c11fcc67e6a213e57fb69ece3
Quentin THIERRY <qthierry@bess-f2r3s5.clusters.42paris.fr>
2023-10-13 13:05:30 +0200

    [NORM] print_error.c

--- Cub3d_bonus/src/utils/print_error.c
+++ Cub3d_bonus/src/utils/print_error.c
@@ -3,15 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   print_error.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jvigny <jvigny@student.42.fr>              +#+  +:+       +#+        */
+/*   By: qthierry <qthierry@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 16:19:43 by jvigny            #+#    #+#             */
-/*   Updated: 2023/10/12 18:31:43 by jvigny           ###   ########.fr       */
+/*   Updated: 2023/10/13 13:02:39 by qthierry         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d_bonus.h"
-
 
 /**
  * @brief print char * if number is 1 else print perror

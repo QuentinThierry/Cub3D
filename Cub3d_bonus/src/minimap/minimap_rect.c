92e4d377d2b853bb254054d626988a4604e81f6b
Quentin THIERRY <qthierry@bess-f2r3s5.clusters.42paris.fr>
2023-10-13 13:35:30 +0200

    [NORM] minimap_rect.c

--- Cub3d_bonus/src/minimap/minimap_rect.c
+++ Cub3d_bonus/src/minimap/minimap_rect.c
@@ -3,59 +3,76 @@
 /*                                                        :::      ::::::::   */
 /*   minimap_rect.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jvigny <jvigny@student.42.fr>              +#+  +:+       +#+        */
+/*   By: qthierry <qthierry@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 16:56:06 by qthierry          #+#    #+#             */
-/*   Updated: 2023/10/11 18:25:51 by jvigny           ###   ########.fr       */
+/*   Updated: 2023/10/13 13:35:08 by qthierry         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d_bonus.h"
 
-static const t_vector2	g_minimap_size =
-(t_vector2)
+static const t_vector2	g_minimap_size
+	= (t_vector2)
 {
 	((WIN_X <= WIN_Y) * WIN_X + (WIN_X > WIN_Y) * WIN_Y) * MINIMAP_SIZE,
 	((WIN_X <= WIN_Y) * WIN_X + (WIN_X > WIN_Y) * WIN_Y) * MINIMAP_SIZE
 };
 
-static const int	g_show_case_range =
-	((WIN_X <= WIN_Y) * WIN_X + (WIN_X > WIN_Y) * WIN_Y) * MINIMAP_SIZE / MMAP_CHUNK / 2 + 1;
+static const int		g_show_case_range
+	= ((WIN_X <= WIN_Y) * WIN_X + (WIN_X > WIN_Y)
+		* WIN_Y) * MINIMAP_SIZE / MMAP_CHUNK / 2 + 1;
+
+static void	draw_minimap_cell(int x, int y, t_game *game)
+{
+	const t_map		**map = (const t_map **)game->map;
+	const t_minimap	*minimap = (const t_minimap *)game->minimap;
+	t_pixel32		color;
+
+	if (x < 0)
+		x = 0;
+	if ((map[y][x].type & DOOR) == DOOR && (map[y][x].type & WALL) != WALL)
+		color = MINIMAP_DOOROPEN_COLOR;
+	else if ((map[y][x].type & WALL) == WALL)
+	{
+		if ((map[y][x].type & DOOR) == DOOR)
+			color = MINIMAP_DOORCLOSE_COLOR;
+		else
+			color = MINIMAP_WALL_COLOR;
+	}
+	else
+		color = MINIMAP_BACKGROUND_COLOR;
+	draw_rectangle(minimap->image,
+		(t_vector2){
+		(x - game->player->f_pos.x) * (MMAP_CHUNK
+			+ (int)minimap->zoom - ZOOM_OFFSET) + g_minimap_size.x / 2.0,
+		(y - game->player->f_pos.y) * (MMAP_CHUNK
+			+ (int)minimap->zoom - ZOOM_OFFSET) + g_minimap_size.y / 2.0},
+		(t_vector2){MMAP_CHUNK + minimap->zoom - ZOOM_OFFSET + 1,
+		MMAP_CHUNK + minimap->zoom - ZOOM_OFFSET + 1}, color);
+}
 
 void	draw_rectangular_minimap(t_game *game)
 {
 	int				x;
 	int				y;
-	t_pixel32		color;
-	const t_map		**map = (const t_map **)game->map;
 	const t_minimap	*minimap = (const t_minimap *)game->minimap;
 
-	y = game->player->f_pos.y - (g_show_case_range - (minimap->zoom / 2 - ZOOM_OFFSET));
-	while (y < game->player->f_pos.y + (g_show_case_range - (minimap->zoom / 2 - ZOOM_OFFSET)) && y < game->map_size.y)
+	y = game->player->f_pos.y
+		- (g_show_case_range - (minimap->zoom / 2 - ZOOM_OFFSET));
+	while (y < game->player->f_pos.y
+		+ (g_show_case_range - (minimap->zoom / 2 - ZOOM_OFFSET))
+		&& y < game->map_size.y)
 	{
 		if (y < 0)
 			y = 0;
-		x = game->player->f_pos.x - (g_show_case_range - (minimap->zoom / 2 - ZOOM_OFFSET));
-		while (x < game->player->f_pos.x + (g_show_case_range - (minimap->zoom / 2 - ZOOM_OFFSET)) && x < game->map_size.x)
+		x = game->player->f_pos.x
+			- (g_show_case_range - (minimap->zoom / 2 - ZOOM_OFFSET));
+		while (x < game->player->f_pos.x
+			+ (g_show_case_range - (minimap->zoom / 2 - ZOOM_OFFSET))
+			&& x < game->map_size.x)
 		{
-			if (x < 0)
-				x = 0;
-			if ((map[y][x].type & DOOR) == DOOR && (map[y][x].type & WALL) != WALL)
-				color = MINIMAP_DOOROPEN_COLOR;
-			else if ((map[y][x].type & WALL) == WALL)
-			{
-				if ((map[y][x].type & DOOR) == DOOR)
-					color = MINIMAP_DOORCLOSE_COLOR;
-				else
-					color = MINIMAP_WALL_COLOR;
-			}
-			else
-				color = MINIMAP_BACKGROUND_COLOR;
-			draw_rectangle(minimap->image,
-				(t_vector2){
-				(x - game->player->f_pos.x) * (MMAP_CHUNK + (int)minimap->zoom - ZOOM_OFFSET) + g_minimap_size.x / 2.0,
-				(y - game->player->f_pos.y) * (MMAP_CHUNK + (int)minimap->zoom - ZOOM_OFFSET) + g_minimap_size.y / 2.0},
-				(t_vector2){MMAP_CHUNK + minimap->zoom - ZOOM_OFFSET + 1, MMAP_CHUNK + minimap->zoom - ZOOM_OFFSET + 1}, color);
+			draw_minimap_cell(x, y, game);
 			x++;
 		}
 		y++;

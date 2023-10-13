041de71ce726dbc64105a80aa1f92dead5ec165d
J0hann3 <jvigny@student.42.fr>
2023-10-13 17:20:07 +0200

    [NORM] the directory hooks

--- Cub3d_bonus/src/hooks/ft_close.c
+++ Cub3d_bonus/src/hooks/ft_close.c
@@ -6,7 +6,7 @@
 /*   By: jvigny <jvigny@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 18:30:39 by jvigny            #+#    #+#             */
-/*   Updated: 2023/10/11 15:34:30 by jvigny           ###   ########.fr       */
+/*   Updated: 2023/10/13 17:03:35 by jvigny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,36 +40,10 @@ void	free_menu(void *mlx_ptr, t_menu *menu)
 	free(menu);
 }
 
-int	ft_close(t_game *game)
+static void	_free_mlx(t_game *game)
 {
 	int	i;
 
-	close_audio(game->music_array);
-	if (game->file_music != NULL)
-		free_music_file(game->file_music, game->nb_music);
-	if (game->object_array != NULL)
-		free_tab_object(game->object_array, game->nb_objects);
-	if (game->player != NULL)
-		free(game->player);
-	if (game->map != NULL)
-		free_map((void *)game->map, game->map_size);
-	if (game->filename != NULL)
-		free_filename(game);
-	free(game->dist_tab);
-	free(game->height_tab);
-	free(game->door_array);
-	free(game->keybinds);
-	if (game->filename)
-		free_filename(game);
-	free_minimap(game->minimap, game->mlx_ptr);
-	free_menu(game->mlx_ptr, game->menu);
-	free_loading_screen(game);
-	free_image(game->mlx_ptr, game->font);
-	free_image(game->mlx_ptr, game->subtitle_font);
-	free_image(game->mlx_ptr, game->image);
-	if (game->end != NULL)
-		free_image(game->mlx_ptr, game->end->end_screen);
-	free(game->end);
 	if (game->mlx_ptr)
 	{
 		mlx_do_key_autorepeaton(game->mlx_ptr);
@@ -86,6 +60,42 @@ int	ft_close(t_game *game)
 		mlx_destroy_display(game->mlx_ptr);
 		free(game->mlx_ptr);
 	}
+}
+
+static void	_ft_close_bis(t_game *game)
+{
+	if (game->file_music != NULL)
+		free_music_file(game->file_music, game->nb_music);
+	if (game->object_array != NULL)
+		free_tab_object(game->object_array, game->nb_objects);
+	if (game->player != NULL)
+		free(game->player);
+	if (game->map != NULL)
+		free_map((void *)game->map, game->map_size);
+	if (game->filename != NULL)
+		free_filename(game);
+	free(game->dist_tab);
+	free(game->height_tab);
+	free(game->door_array);
+	free(game->keybinds);
+}
+
+int	ft_close(t_game *game)
+{
+	close_audio(game->music_array);
+	_ft_close_bis(game);
+	if (game->filename)
+		free_filename(game);
+	free_minimap(game->minimap, game->mlx_ptr);
+	free_menu(game->mlx_ptr, game->menu);
+	free_loading_screen(game);
+	free_image(game->mlx_ptr, game->font);
+	free_image(game->mlx_ptr, game->subtitle_font);
+	free_image(game->mlx_ptr, game->image);
+	if (game->end != NULL)
+		free_image(game->mlx_ptr, game->end->end_screen);
+	free(game->end);
+	_free_mlx(game);
 	if (nb_fps != 0)
 		printf("Moyenne fps : %ld\n", (long)tot_fps / nb_fps);
 	exit(0);
--- Cub3d_bonus/src/hooks/key_hook.c
+++ Cub3d_bonus/src/hooks/key_hook.c
@@ -6,11 +6,33 @@
 /*   By: jvigny <jvigny@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 17:26:14 by jvigny            #+#    #+#             */
-/*   Updated: 2023/10/11 18:26:07 by jvigny           ###   ########.fr       */
+/*   Updated: 2023/10/13 17:18:02 by jvigny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d_bonus.h"
+
+static void	_key_press_bis(t_keybind key, t_game *game)
+{
+	if (key == game->keybinds[e_key_interact_door])
+		open_door(game);
+	else if (key == game->keybinds[e_key_pause])
+		set_pause_menu_mode(game);
+	else if (key == 't')
+	{
+		game->fov += 1;
+		game->constants[0] = (WIN_X / 2.) / tan((game->fov / 2.) * TO_RADIAN);
+		game->constants[1] = tanf((game->fov / 2.0) * TO_RADIAN);
+		game->constants[2] = cos((game->fov / 2.0) * TO_RADIAN);
+	}
+	else if (key == 'y')
+	{
+		game->fov -= 1;
+		game->constants[0] = (WIN_X / 2.) / tan((game->fov / 2.) * TO_RADIAN);
+		game->constants[1] = tanf((game->fov / 2.0) * TO_RADIAN);
+		game->constants[2] = cos((game->fov / 2.0) * TO_RADIAN);
+	}
+}
 
 void	key_press_hook(t_keybind key, t_game *game)
 {
@@ -34,24 +56,7 @@ void	key_press_hook(t_keybind key, t_game *game)
 		game->minimap->zoom_dir -= 1;
 	else if (key == game->keybinds[e_key_minimap_zoom])
 		game->minimap->zoom_dir += 1;
-	else if (key == game->keybinds[e_key_interact_door])
-		open_door(game);
-	else if (key == game->keybinds[e_key_pause])
-		set_pause_menu_mode(game);
-	else if (key == 't')
-	{
-		game->fov += 1;
-		game->constants[0] = (WIN_X / 2.) / tan((game->fov / 2.) * TO_RADIAN);
-		game->constants[1] = tanf((game->fov / 2.0) * TO_RADIAN);
-		game->constants[2] = cos((game->fov / 2.0) * TO_RADIAN);
-	}
-	else if (key == 'y')
-	{
-		game->fov -= 1;
-		game->constants[0] = (WIN_X / 2.) / tan((game->fov / 2.) * TO_RADIAN);
-		game->constants[1] = tanf((game->fov / 2.0) * TO_RADIAN);
-		game->constants[2] = cos((game->fov / 2.0) * TO_RADIAN);
-	}
+	_key_press_bis(key, game);
 }
 
 int	exit_hook(int key, t_game *game)
@@ -81,91 +86,4 @@ void	key_release_hook(t_keybind key, t_game *game)
 		game->minimap->zoom_dir += 1;
 	else if (key == game->keybinds[e_key_minimap_zoom])
 		game->minimap->zoom_dir -= 1;
-}
-
-void	player_move(t_game *game, t_player *player, double delta_time, t_map **map)
-{
-	t_dvector2	move_value;
-
-	if (player->view != 0)
-		player->angle += ROTATION_KEYBOARD * delta_time * player->view;
-	move_value.x = 0;
-	move_value.y = 0;
-	if (player->dir.y != 0)
-	{
-		move_value.x -= sinf(player->angle * TO_RADIAN) * player->speed * player->dir.y;
-		move_value.y += cosf(player->angle * TO_RADIAN) * player->speed * player->dir.y;
-	}
-	if (player->dir.x != 0)
-	{
-		move_value.x += cosf(player->angle * TO_RADIAN) * player->speed * player->dir.x;
-		move_value.y += sinf(player->angle * TO_RADIAN) * player->speed * player->dir.x;
-	}
-	if (player->dir.x != 0 && player->dir.y != 0)
-	{
-		move_value.x *= 0.707;
-		move_value.y *= 0.707;
-	}
-	if (player->dir.x != 0 || player->dir.y != 0)
-	{
-		move_value.x = player->f_pos.x + move_value.x * delta_time;
-		move_value.y = player->f_pos.y + move_value.y * delta_time;
-		move_value = check_colliding(game, move_value, map);
-		if (((int)move_value.x != (int)player->f_pos.x || (int)move_value.y != (int)player->f_pos.y)
-			&& (map[(int)move_value.y][(int)move_value.x].type & MUSIC) == MUSIC)
-			play_music(&map[(int)move_value.y][(int)move_value.x],
-				game->music_array, map[(int)move_value.y][(int)move_value.x].music, IS_PLAYING_MUSIC);
-		if (((int)move_value.x != (int)player->f_pos.x || (int)move_value.y != (int)player->f_pos.y)
-			&& (map[(int)move_value.y][(int)move_value.x].type & NARRATOR) == NARRATOR)
-		{
-			play_narrator(game, &map[(int)move_value.y][(int)move_value.x], game->music_array);
-			map[(int)move_value.y][(int)move_value.x].type ^= NARRATOR;
-		}
-		game->player->f_pos = move_value;
-	}
-}
-
-int	mouse_hook(int x, int y, t_game *game)
-{
-	game->player->angle -= (double)(game->player->mouse_pos.x - x) / ROTATION_MOUSE;
-	if (x != WIN_X / 2 || y != WIN_Y / 2)
-	{
-		mlx_mouse_move(game->mlx_ptr, game->win, WIN_X / 2, WIN_Y / 2);
-		game->player->mouse_pos.x = WIN_X / 2;
-		game->player->mouse_pos.y = WIN_Y / 2;
-	}
-	return (0);
-}
-
-int	mouse_stay_in_window_hook(int x, int y, t_game *game)
-{
-	if (x != WIN_X / 2 || y != WIN_Y / 2)
-	{
-		mlx_mouse_move(game->mlx_ptr, game->win, WIN_X / 2, WIN_Y / 2);
-		game->player->mouse_pos.x = WIN_X / 2;
-		game->player->mouse_pos.y = WIN_Y / 2;
-	}
-	return (0);
-}
-
-int	mouse_leave(t_game *game)
-{
-	mlx_mouse_move(game->mlx_ptr, game->win, WIN_X / 2, WIN_Y / 2);
-	game->player->mouse_pos.x = WIN_X / 2;
-	game->player->mouse_pos.y = WIN_Y / 2;
-	return (0);
-}
-
-int	mouse_click(int button, int x, int y, t_game *game)
-{
-	(void)x;
-	(void)y;
-	if (button == 1)
-	{
-		if (game->player->has_item == true)
-			drop_object(game->player, game->map, game->exit, game);
-		else
-			take_object_click(game, game->player, game->map);
-	}
-	return (0);
 }

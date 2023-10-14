a67243387e0a760f42a872b68066d2e27701714d
J0hann3 <jvigny@student.42.fr>
2023-10-14 16:34:20 +0200

    [NORM] all file wall_hit

--- Cub3d_bonus/src/raycasting/wall_hit.c
+++ Cub3d_bonus/src/raycasting/wall_hit.c
@@ -6,7 +6,7 @@
 /*   By: jvigny <jvigny@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/30 16:04:05 by qthierry          #+#    #+#             */
-/*   Updated: 2023/10/14 15:51:47 by jvigny           ###   ########.fr       */
+/*   Updated: 2023/10/14 16:27:39 by jvigny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,118 +16,10 @@ t_ray	get_wall_hit_se(t_dvector2 fpos, t_map **map, float angle,
 			float player_angle);
 t_ray	get_wall_hit_ne(t_dvector2 fpos, t_map **map, float angle,
 			float player_angle);
-
-// xy (-1, 1)
-static t_ray	_get_wall_hit_sw(t_dvector2 fpos,
-								t_map **map, float angle, float player_angle)
-{
-	t_fvector2	step;
-	t_dvector2	comp;
-	t_dvector2	door;
-	t_vector2	map_pos;
-
-	map_pos = (t_vector2){(int)fpos.x, (int)fpos.y + 1};
-	comp.x = fpos.x + fabs(fpos.y - map_pos.y) * angle * -1;
-	comp.y = fpos.y + fabs(fpos.x - map_pos.x) / angle;
-	step = (t_fvector2){angle * -1, 1 / angle};
-	while (true)
-	{
-		if (map_pos.y >= comp.y)
-		{
-			if ((map[(int)(comp.y)][map_pos.x - 1].type & OBJECT) == OBJECT)
-				((t_object *)map[(int)(comp.y)][map_pos.x - 1].arg)->visited = true;
-			else if ((map[(int)(comp.y)][map_pos.x - 1].type & WALL) == WALL)
-			{
-				if ((map[(int)(comp.y)][map_pos.x - 1].type & DOOR) == DOOR)
-				{
-					door = door_hit_ver_sw((t_dvector2){map_pos.x, comp.y}, step.y,
-							(float)((t_door *)map[(int)(comp.y)][map_pos.x - 1].arg)->door_percent, player_angle);
-					if (door.x >= 0)
-						return ((t_ray){door, e_east});
-				}
-				else
-					return ((t_ray){{map_pos.x, comp.y}, e_east});
-			}
-			comp.y += step.y;
-			map_pos.x -= 1;
-		}
-		else
-		{
-			if ((map[map_pos.y][(int)(comp.x)].type & OBJECT) == OBJECT)
-				((t_object *)map[map_pos.y][(int)(comp.x)].arg)->visited = true;
-			else if ((map[map_pos.y][(int)(comp.x)].type & WALL) == WALL)
-			{
-				if ((map[map_pos.y][(int)(comp.x)].type & DOOR) == DOOR)
-				{
-					door = door_hit_hor_sw((t_dvector2){comp.x, map_pos.y}, step.x,
-							(float)((t_door *)map[map_pos.y][(int)comp.x].arg)->door_percent, player_angle);
-					if (door.x >= 0)
-						return ((t_ray){door, e_north});
-				}
-				else
-					return ((t_ray){{comp.x, map_pos.y}, e_north});
-			}
-			comp.x += step.x;
-			map_pos.y += 1;
-		}
-	}
-}
-
-// xy (-1, -1)
-static t_ray	_get_wall_hit_nw(t_dvector2 fpos,
-								t_map **map, float angle, float player_angle)
-{
-	t_fvector2	step;
-	t_dvector2	comp;
-	t_dvector2	door;
-	t_vector2	map_pos;
-
-	map_pos = (t_vector2){(int)fpos.x, (int)fpos.y};
-	comp.x = fpos.x + fabs(fpos.y - map_pos.y) * angle * -1;
-	comp.y = fpos.y + fabs(fpos.x - map_pos.x) / angle * -1;
-	step = (t_fvector2){angle * -1, 1 / angle * -1};
-	while (true)
-	{
-		if (map_pos.y <= comp.y)
-		{
-			if ((map[(int)(comp.y)][map_pos.x - 1].type & OBJECT) == OBJECT)
-				((t_object *)map[(int)(comp.y)][map_pos.x - 1].arg)->visited = true;
-			else if ((map[(int)(comp.y)][map_pos.x - 1].type & WALL) == WALL)
-			{
-				if ((map[(int)(comp.y)][map_pos.x - 1].type & DOOR) == DOOR)
-				{
-					door = door_hit_ver_nw((t_dvector2){map_pos.x, comp.y}, step.y,
-							(float)((t_door *)map[(int)(comp.y)][map_pos.x - 1].arg)->door_percent, player_angle);
-					if (door.x >= 0)
-						return ((t_ray){door, e_east});
-				}
-				else
-					return ((t_ray){{map_pos.x, comp.y}, e_east});
-			}
-			comp.y += step.y;
-			map_pos.x -= 1;
-		}
-		else
-		{
-			if ((map[map_pos.y - 1][(int)(comp.x)].type & OBJECT) == OBJECT)
-				((t_object *)map[map_pos.y - 1][(int)(comp.x)].arg)->visited = true;
-			else if ((map[map_pos.y - 1][(int)(comp.x)].type & WALL) == WALL)
-			{
-				if ((map[map_pos.y - 1][(int)(comp.x)].type & DOOR) == DOOR)
-				{
-					door = door_hit_hor_nw((t_dvector2){comp.x, map_pos.y}, step.x,
-							(float)((t_door *)map[map_pos.y - 1][(int)comp.x].arg)->door_percent, player_angle);
-					if (door.x >= 0)
-						return ((t_ray){door, e_south});
-				}
-				else
-					return ((t_ray){{comp.x, map_pos.y}, e_south});
-			}
-			comp.x += step.x;
-			map_pos.y += -1;
-		}
-	}
-}
+t_ray	get_wall_hit_sw(t_dvector2 fpos, t_map **map, float angle,
+			float player_angle);
+t_ray	get_wall_hit_nw(t_dvector2 fpos, t_map **map, float angle,
+			float player_angle);
 
 t_ray	get_wall_hit(t_dvector2 fpos, t_map **map, float angle)
 {
@@ -143,9 +35,9 @@ t_ray	get_wall_hit(t_dvector2 fpos, t_map **map, float angle)
 		return (get_wall_hit_ne(fpos, map, fabsf(tanf(angle * TO_RADIAN)),
 				angle));
 	else if (sign.x == -1 && sign.y == 1)
-		return (_get_wall_hit_sw(fpos, map, fabsf(tanf(angle * TO_RADIAN)),
+		return (get_wall_hit_sw(fpos, map, fabsf(tanf(angle * TO_RADIAN)),
 				angle));
 	else
-		return (_get_wall_hit_nw(fpos, map, fabsf(tanf(angle * TO_RADIAN)),
+		return (get_wall_hit_nw(fpos, map, fabsf(tanf(angle * TO_RADIAN)),
 				angle));
 }
--- Cub3d_bonus/src/raycasting/wall_hit_ne.c
+++ Cub3d_bonus/src/raycasting/wall_hit_ne.c
@@ -6,15 +6,15 @@
 /*   By: jvigny <jvigny@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 15:36:17 by jvigny            #+#    #+#             */
-/*   Updated: 2023/10/14 15:51:17 by jvigny           ###   ########.fr       */
+/*   Updated: 2023/10/14 16:31:13 by jvigny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d_bonus.h"
 
 __attribute__((always_inline))
-static inline t_ray	_wall_hit_ver_ne(t_map *map_cell, t_dvector2 *hit, float step,
-					float player_angle)
+static inline t_ray	_wall_hit_ver_ne(t_map *map_cell, t_dvector2 *hit,
+					float step, float player_angle)
 {
 	t_dvector2	door;
 
@@ -25,12 +25,13 @@ static inline t_ray	_wall_hit_ver_ne(t_map *map_cell, t_dvector2 *hit, float ste
 		if ((map_cell->type & DOOR) == DOOR)
 		{
 			door = door_hit_ver_ne(*hit, step,
-					(float)((t_door *)map_cell->arg)->door_percent, player_angle);
+					(float)((t_door *)map_cell->arg)->door_percent,
+					player_angle);
 			if (door.x >= 0)
 				return ((t_ray){door, e_west});
 		}
 		else
-			return ((t_ray){*hit, e_west});
+			return ((t_ray){(*hit), e_west});
 	}
 	hit->x += 1;
 	hit->y += step;
@@ -38,8 +39,8 @@ static inline t_ray	_wall_hit_ver_ne(t_map *map_cell, t_dvector2 *hit, float ste
 }
 
 __attribute__((always_inline))
-static inline t_ray	_wall_hit_hor_ne(t_map *map_cell, t_dvector2 *hit, float step,
-					float player_angle)
+static inline t_ray	_wall_hit_hor_ne(t_map *map_cell, t_dvector2 *hit,
+						float step, float player_angle)
 {
 	t_dvector2	door;
 
@@ -50,12 +51,13 @@ static inline t_ray	_wall_hit_hor_ne(t_map *map_cell, t_dvector2 *hit, float ste
 		if ((map_cell->type & DOOR) == DOOR)
 		{
 			door = door_hit_hor_ne(*hit, step,
-					(float)((t_door *)map_cell->arg)->door_percent, player_angle);
+					(float)((t_door *)map_cell->arg)->door_percent,
+					player_angle);
 			if (door.x >= 0)
 				return ((t_ray){door, e_south});
 		}
 		else
-			return ((t_ray){*hit, e_south});
+			return ((t_ray){(*hit), e_south});
 	}
 	hit->x += step;
 	hit->y += -1;
@@ -79,14 +81,14 @@ t_ray	get_wall_hit_ne(t_dvector2 fpos, t_map **map, float angle,
 		if (hor.y <= ver.y)
 		{
 			ray = _wall_hit_ver_ne(&map[(int)ver.y][(int)ver.x],
-				&ver, 1 / angle * -1, player_angle);
+					&ver, 1 / angle * -1, player_angle);
 			if (ray.hit.x != -1)
 				return (ray);
 		}
 		else
 		{
 			ray = _wall_hit_hor_ne(&map[(int)hor.y - 1][(int)hor.x],
-				&hor, angle, player_angle);
+					&hor, angle, player_angle);
 			if (ray.hit.x != -1)
 				return (ray);
 		}

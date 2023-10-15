6b91158f958db998248c58741cf66354551b69e6
Quentin THIERRY <qthierry@bess-f1r5s10.clusters.42paris.fr>
2023-10-15 20:37:26 +0200

    [ADD] hide mouse (uncomment line init_mouse.c)

--- Cub3d_bonus/src/parsing/init_mouse.c
+++ Cub3d_bonus/src/parsing/init_mouse.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init_mouse.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jvigny <jvigny@student.42.fr>              +#+  +:+       +#+        */
+/*   By: qthierry <qthierry@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 17:49:23 by jvigny            #+#    #+#             */
-/*   Updated: 2023/10/15 18:27:20 by jvigny           ###   ########.fr       */
+/*   Updated: 2023/10/15 20:36:50 by qthierry         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	move_mouse(t_game *game)
 void	init_mouse(t_game *game)
 {
 	mlx_mouse_move(game->mlx_ptr, game->win, WIN_X / 2, WIN_Y / 2);
-	// mlx_mouse_hide(game->mlx_ptr, game->win);
+	mlx_mouse_hide(game->mlx_ptr, game->win);
 	game->player->mouse_pos.x = WIN_X / 2;
 	game->player->mouse_pos.y = WIN_Y / 2;
 }
